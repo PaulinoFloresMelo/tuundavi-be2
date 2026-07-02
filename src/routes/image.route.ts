5cbfea35cec6653586360f3c95409c9989a0935d
Paulino Flores Melo <PaulinoFloresMelo@users.noreply.github.com>
2026-07-02 17:24:04 -0600

    update image path in wrangler

--- src/routes/image.route.ts
+++ src/routes/image.route.ts
@@ -5,8 +5,8 @@ export const imageRouter = factory.createApp()
 imageRouter.get('/:image', async (c) => {
   const imageName = c.req.param('image')
 
-  const assetPath = `public/static/images/${imageName}`
-  const assetRequest = new Request(new URL(assetPath, c.req.url))
+  const assetPath = `/static/images/${imageName}`
+  const assetRequest = new Request(new URL( assetPath, c.req.url ))
 
   const assetResponse = await c.env.ASSETS.fetch(assetRequest)
 
